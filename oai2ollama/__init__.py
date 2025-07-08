a8e145e90c865d9a5f600d78e045f1a58090d8cb
Muspi Merol <me@promplate.dev>
2025-07-08 22:52:27 +0800

    set host to `localhost` to listen both IPv4 and IPv6

--- oai2ollama/__init__.py
+++ oai2ollama/__init__.py
@@ -4,7 +4,7 @@ from ._app import app
 def start():
     import uvicorn
 
-    uvicorn.run(app, host="::", port=11434)
+    uvicorn.run(app, host="localhost", port=11434)
 
 
 __all__ = ["_app", "start"]
